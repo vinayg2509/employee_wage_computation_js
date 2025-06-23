3fca9d87d7c92e709c89ac8b76bbb954195ffb30
vinayg2509 <vinayg2509@gmail.com>
2025-06-23 11:55:07 +0530

    UC1: Add logic to check employee attendance using random()

--- employee.js
+++ employee.js
@@ -1,1 +1,29 @@
 console.log("Welcome to employee wage computation");
+
+
+// ! UC1-CheckAttendance
+
+class employee{
+
+    constructor(empId,empName)
+    {
+        this.empId=empId
+        this.empName=empName
+        this.empAttendance=this.checkAttendance()
+    }
+
+
+    checkAttendance()
+    {
+        return Math.floor(Math.random()*2)?"Absent":"Present"
+    }
+
+    displayDetails()
+    {
+        console.log(`Employee ID :${this.empId} and Employee name :${this.empName} is ${this.empAttendance}`);
+        
+    }
+}
+
+let emp1=new employee(1455,"Tom")
+emp1.displayDetails()
